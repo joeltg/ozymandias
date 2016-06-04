5ed64ee9ee6e50cc2d0d4f0579ce57cad71af7b5
Joel Gustafson <joelgustafson@Joels-MacBook-Pro.local>
2016-06-03 23:02:31 -0400

    removed arrow functions

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ const server = new WebSocketServer({ port: 1947 });
 console.log('listening on port 1947');
 
 const children  = [];
-server.on('connection', socket =>  {
+server.on('connection', function(socket)  {
 
     const scheme = spawn('mit-scheme');
     scheme.stdout.setEncoding('utf8');
     children.push(scheme);
 
-    scheme.on('close', (code, signal) => console.log('scheme closed', signal));
+    scheme.on('close', function(code, signal) {
+      console.log('scheme closed', signal));
+    }
 
-    scheme.stdout.on('data', data => {
+    scheme.stdout.on('data', function(data) {
         process.stdout.write(data.toString());
         socket.send(data.toString());
     });
 
-    socket.on('message', message => {
+    socket.on('message', function(message) {
         if (message === "\<INTERRUPT\>")
             scheme.kill("SIGINT");
         else if (message === "\<KILL\>") {
@@ -36,7 +38,7 @@ server.on('connection', socket =>  {
         else scheme.stdin.write(message);
     });
 
-    socket.on('close', () => {
+    socket.on('close', function() {
         scheme.kill();
         children.splice(children.indexOf(socket), 1);
     });
@@ -49,6 +51,5 @@ process.on('SIGTERM', cleanExit); // catch kill
 
 process.on('exit', function() {
     console.log('killing', children.length, 'child processes');
-    children.forEach(child => child.kill());
+    children.forEach(function(child) { child.kill() });
 });
-
