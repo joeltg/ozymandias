4eac17096c76c13a9f51d54215b967ee034e2067
Joel Gustafson <joelg@mit.edu>
2016-08-20 11:59:42 -0400

    initial refactor

--- web/src/connect-to-server.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by joel on 7/12/16.
- */
-
-// Replace with the URL of your server
-const websocket_url = 'ws://localhost:1947';
-// const websocket_url = 'ws://maharal.csail.mit.edu:1947';
