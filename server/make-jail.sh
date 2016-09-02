69bd9eab0276335b086400bb6d16ce625db0ead7
Joel Gustafson <joelg@mit.edu>
2016-09-02 05:18:53 -0400

    fixed path in make-jail

--- server/make-jail.sh
+++ server/make-jail.sh
@@ -14,7 +14,7 @@ echo "/etc/resolv.conf" > /etc/schroot/scheme/copyfiles
 echo "${dir}/root/bin   /bin    none ro,bind 0 0" >  /etc/schroot/scheme/fstab
 echo "${dir}/root/lib   /lib    none ro,bind 0 0" >> /etc/schroot/scheme/fstab
 echo "${dir}/root/lib64 /lib64  none ro,bind 0 0" >> /etc/schroot/scheme/fstab
-echo "${dir}utils       /utils  none ro,bind 0 0" >> /etc/schroot/scheme/fstab
+echo "${dir}/utils       /utils  none ro,bind 0 0" >> /etc/schroot/scheme/fstab
 echo "${scheme}         /scheme none ro,bind 0 0" >> /etc/schroot/scheme/fstab
 
 echo "" > /etc/schroot/scheme/nssdatabases
