d50d85df26217d2a7eaeac819a0d59d9157012ac
Joel Gustafson <joelg@mit.edu>
2016-09-20 03:33:38 -0400

    updated render expression procedure to take no required arguments

--- utils/latex/latex-window.scm
+++ utils/latex/latex-window.scm
@@ -1,3 +1,5 @@
+(define latex-environment (the-environment))
+
 (define-structure (latex-window (constructor silently-make-latex-window (#!optional name)))
   (name (string-append "latex-window-" (number->string (get-id))) read-only #t))
 
@@ -12,14 +14,25 @@
                 (type latex)
                 (latex ,(if (default-object? latex) "" latex)))))
 
-(define (re expression #!optional latex-window)
-  (let ((latex-window (if (latex-window? latex-window) latex-window (silently-make-latex-window)))
-        (expression (cond ((literal-number? expression) (simplify expression))
-                           ((literal-function? expression) (simplify expression))
-                           (else expression))))
-    (send-latex-window latex-window (expression->tex-string expression) #t)
-    latex-window))
+(define (render-expression #!optional expression latex-window)
+  (if (default-object? expression)
+      (if (or (undefined-value? (*last-expression-printed*))
+              (and (procedure? (*last-expression-printed*))
+                   (not (operator? (*last-expression-printed*)))))
+            (*last-expression-printed*)
+            (render-expression (*last-expression-printed*) latex-window))
+      (let ((latex-window (if (latex-window? latex-window) latex-window (silently-make-latex-window)))
+            (expression (cond ((literal-number? expression) (simplify expression))
+                               ((literal-function? expression) (simplify expression))
+                               (else expression))))
+        (send-latex-window latex-window (expression->tex-string expression) #t)
+        expression)))
+
+(define re render-expression)
+
+(define last-latex-window #f)
 
 (define (send-latex-window latex-window #!optional latex push)
   (assert (latex-window? latex-window))
+  (set! last-latex-window latex-window)
   (send-json (latex-window->json latex-window latex) push))
